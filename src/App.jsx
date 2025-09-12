52016eae0122820d78bcb337743ea79d03caea34
Alex-Alessi <alex.alessi246@gmail.com>
2025-09-12 12:31:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
